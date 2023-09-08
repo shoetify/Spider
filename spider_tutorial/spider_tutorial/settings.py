c8a7ac4916934e199b3fe92b7f41c6856085e7d4
shoetify <shoetify@gmail.com>
2023-09-08 20:22:24 +0800

    Scrapy using - user agent changing

--- spider_tutorial/spider_tutorial/settings.py
+++ spider_tutorial/spider_tutorial/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'spider_tutorial.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'spider_tutorial (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
